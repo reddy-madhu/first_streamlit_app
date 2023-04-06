cb95639520a591645345d37498a3973fe1dc0fc6
Madhushree Manjunath Reddy <128741337+reddy-madhu@users.noreply.github.com>
2023-04-05 19:53:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🐔 Hard-Boiled free-range egg')
 streamlit.text('🥑🍞 Avocado Toast ')
                 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+Import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
