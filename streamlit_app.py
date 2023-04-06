75b333fde3f484e74edde60535ae3e0530922e16
Madhushree Manjunath Reddy <128741337+reddy-madhu@users.noreply.github.com>
2023-04-05 19:45:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
+
 streamlit.title('My Parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry Oatmeal')
+streamlit.text('Lake, Spinach & rocket smoothie')
+streamlit.text('Hard-Boiled free-range egg')
