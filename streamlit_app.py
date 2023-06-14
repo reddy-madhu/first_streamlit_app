722d153d81eb446084ea34e47844963cd9ef97c9
Madhushree Manjunath Reddy <128741337+reddy-madhu@users.noreply.github.com>
2023-06-14 15:39:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
 #streamlit.stop()
 
 
-streamlit.text("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add your Favorites!")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
